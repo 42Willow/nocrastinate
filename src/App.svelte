9cc7b70bab9b212167ab4028369f3e2eae55efe5
42willow <70784752+42Willow@users.noreply.github.com>
2023-07-30 16:55:58 +1000

    Still broken

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
-<div class="daisy-mockup-browser border bg-base-300">
-    <div class="daisy-mockup-browser-toolbar">
-      <div class="daisy-input">https://daisyui.com</div>
+<div class="mockup-browser border bg-base-300">
+    <div class="mockup-browser-toolbar">
+      <div class="input">https://daisyui.com</div>
     </div>
     <div class="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
-  </div>
+</div>
+
+<span class="daisy-loading daisy-loading-spinner daisy-loading-xs"></span>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,4 @@
 module.exports = {
   content: ['./src/**/*.{svelte,js,}'],
   plugins: [require('daisyui')],
-  daisyui: {
-    prefix: "daisy-", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-  },
 };
