a227e822cb48ed20f25b1ce0063ac4b515c78d7a
42willow <70784752+42Willow@users.noreply.github.com>
2023-07-31 18:51:30 +1000

    Update 404 styling

--- src/pages/NotFound.svelte
+++ src/pages/NotFound.svelte
@@ -1,1 +1,3 @@
-<object type="image/svg+xml" data="404.svg" title="404 Not Found" />
+<div class="flex items-center justify-center h-screen" style="background-color: #262a37;">
+  <object type="image/svg+xml" data="404.svg" title="404 Not Found" class="max-h-screen" />
+</div>
