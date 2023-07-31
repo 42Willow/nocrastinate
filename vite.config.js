989b043ffd2df08a3cd98b9ce89ca9a92cd3335a
42willow <70784752+42Willow@users.noreply.github.com>
2023-07-31 19:41:27 +1000

    404 build config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/nocrastinate/',
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        '404': './404.html',
+      },
+    },
+  },
+  server: {
+    fs: {
+      strict: false,
+      allow: ['..', '404.html'],
+    },
+  },
 });
